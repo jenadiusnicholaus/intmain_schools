57577273ad2246a47089e1c240d3b20d89d5a816
jenadiusnicholaus <ucode888@gmail.com>
2023-01-05 14:31:30 +0300

    fix static file

--- intmain_settings_app/settings.py
+++ intmain_settings_app/settings.py
@@ -133,6 +133,9 @@ MEDIA_URL = "/media/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # STATIC_ROOT = (os.path.join(SITE_ROOT, 'static_files/'))
 

ed35fbff195199dac25342dba1115d5aa0d1f6ca
jenadiusnicholaus <ucode888@gmail.com>
2023-01-05 14:07:45 +0300

    added the database

--- intmain_settings_app/settings.py
+++ intmain_settings_app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "intmain_settings_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "intmaindb",
+        "USER": "intmainadmin",
+        "PASSWORD": "intmain2023",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
